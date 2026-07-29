006851907366b2eff1a9222dee13c636f850dc01
3b3al <51370993+3b3al@users.noreply.github.com>
2026-07-30 01:46:49 +0300

    [7] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 7-reverse-integer/7-Reverse-Integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0){
+            int firstDigit = x % 10;
+            x=x/10;
+            
+            if(reversed > Integer.MAX_VALUE /10 || (reversed == Integer.MAX_VALUE/10 && firstDigit > 7 ))
+                return 0;
+            
+            if(reversed < Integer.MIN_VALUE /10 || (reversed == Integer.MIN_VALUE/10 && firstDigit <-8  ))
+                return 0;
+            
+            reversed  = reversed*10 + firstDigit;
+            
+            
+        }
+        return reversed;
+        
+        
+        
+    }
+}
